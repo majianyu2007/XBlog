81595dc677cdfb5e8ab4b233c07728e855fa97cc
刘禛 <7468120+xiaozhu2007@user.noreply.gitee.com>
2021-02-09 19:12:55 +0800

    紧急修复Bug-01

--- t/m.php
+++ t/m.php
@@ -1,5 +1,2 @@
-<div class='p'>[posts]</div>
-<script>document.title='[name]';</script>
-<div id='loadmore'>
-<a href="javascript:void(0);" onclick='getmore();' class="button-rounded">加载更多</a>
+<div class='p'>[posts]</div><script>document.title='[name]';</script><div id='loadmore'><a href="javascript:void(0);" onclick='getmore();' class="button-rounded">加载更多</a>
 </div>
