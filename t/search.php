4ae18dde6cccd8ade4adf597478b891ee2d62a89
刘禛 <7468120+xiaozhu2007@user.noreply.gitee.com>
2021-02-09 19:17:05 +0800

    紧急修复Bug-03

--- t/search.php
+++ t/search.php
@@ -1,11 +1,4 @@
 <div class='title'>
-	<h3 id='ss'>
-		搜索
-	</h3>
-</div>
-<div class='p'>
-	[searchs]
-</div>
-<script>
-	document.title = SC('ss').innerHTML;
-</script>
+	<h3 id='ss'>搜索</h3>
+</div><div class='p'>[searchs]</div>
+<script>document.title = SC('ss').innerHTML;</script>
--- t/tag.php
+++ t/tag.php
@@ -3,9 +3,5 @@
 		标签页
 	</h3>
 </div>
-<div class='p'>
-	[tags]
-</div>
-<script>
-	document.title = SC('bq').innerHTML;
-</script>
+<div class='p'>[tags]</div>
+<script>document.title = SC('bq').innerHTML;</script>
